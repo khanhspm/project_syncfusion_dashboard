262ce08530e4bff3ab8373a7a00ec819f937ddfa
khanhspm <khanhcshadu@gmail.com>
2023-05-09 10:01:58 +0700

    add kanban

--- src/App.js
+++ src/App.js
@@ -1,11 +1,63 @@
-import React from 'react'
+import './App.css';
+import './components/event.css';
+import './components/task.css';
+import './components/column.css';
+import React, { useMemo, useState, useCallback, useEffect } from 'react';
+import EventBar from './components/EventBar';
+import TaskBox from './components/TaskBox';
 
-import './App.css'
+function App() {
+  const initEvent = useMemo(() => [
+    {
+      title: 'Untitled',
+      'To do': [],
+      'In progress': [],
+      'Completed': [],
+    },
+  ], []);
 
-const App = () => {
+  const [events, setEvents] = useState(() => {
+    return localStorage.getItem('events')
+      ? JSON.parse(localStorage.getItem('events'))
+      : initEvent;
+  });
+
+  const [currentEvent, setCurrentEvent] = useState(events[0]);
+
+  const updateEvents = useCallback(async () => {
+    try {
+      if (!events.length) {
+        await localStorage.setItem('events', JSON.stringify(initEvent));
+        setEvents(JSON.parse(localStorage.getItem('events')));
+      } else {
+        await localStorage.setItem('events', JSON.stringify(events));
+      }
+    } catch (e) {
+      console.error('Failed to modify events!');
+    }
+  }, [events, initEvent]);
+
+  // Set localStorage
+  useEffect(() => {
+    updateEvents();
+  }, [events, updateEvents]);
+
   return (
-    <h1 className='underline text-3xl'>App</h1>
-  )
+    <div className='App'>
+      <EventBar
+        events={events}
+        setEvents={setEvents}
+        currentEvent={currentEvent}
+        setCurrentEvent={setCurrentEvent}
+      />
+      <TaskBox
+        events={events}
+        setEvents={setEvents}
+        currentEvent={currentEvent}
+        setCurrentEvent={setCurrentEvent}
+      />
+    </div>
+  );
 }
 
-export default App
+export default App;
